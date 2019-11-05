43dd4bab4d2e26b979a2470ca614485bcdb85ed7
liulihan <729664907@qq.com>
2019-11-05 15:18:31 +0800

    创建新用户

---
+++ src/com/atguigu/egit/test/HelloEgit.java
@@ -0,0 +1,12 @@
+package com.atguigu.egit.test;
+
+public class HelloEgit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Egit!");
+		System.out.println("Hello Egit 222!");
+		
+
+	}
+
+}
